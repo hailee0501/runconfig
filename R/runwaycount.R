3fa38a1decd741e713a58c89500de8e55d9e21c3
Hui Jeong Ha <huijeongha@Huis-MacBook-Pro.local>
2020-02-25 13:29:48 -0500

    function1

--- R/runwaycount.R
+++ R/runwaycount.R
@@ -1,6 +1,6 @@
 library(dplyr)
 runwaycount<-function(.data){
   x<-.data$airport
-  .data$runwaycount
-  runwaycount<-UseMethod("x%>%group_by(x)%>%summarise(n=n())")
+  .data$RC
+  RC<-data%>%count(airport)
   }
